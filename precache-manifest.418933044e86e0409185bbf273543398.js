afd03cc66ac8f58bd9c49eda446a09ba101700c7
andreaup <niklerk@DESKTOP-0PHNLLB>
2020-09-30 22:15:04 -0500

    Updates

---
+++ precache-manifest.418933044e86e0409185bbf273543398.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71309c5c68cbf6eff857ba2dd42c39fc",
+    "url": "/reward-store/index.html"
+  },
+  {
+    "revision": "7ab963e49da2e8298ec3",
+    "url": "/reward-store/static/css/main.b9a53f76.chunk.css"
+  },
+  {
+    "revision": "9c364861dc9bf97d1170",
+    "url": "/reward-store/static/js/2.6acca1ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reward-store/static/js/2.6acca1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab963e49da2e8298ec3",
+    "url": "/reward-store/static/js/main.c284da61.chunk.js"
+  },
+  {
+    "revision": "941b31aa1efa18ac6b48",
+    "url": "/reward-store/static/js/runtime-main.b19dc24d.js"
+  },
+  {
+    "revision": "71af6f4be21b71f7b37a986aca3a5af6",
+    "url": "/reward-store/static/media/aerolab-logo.71af6f4b.svg"
+  },
+  {
+    "revision": "2f11a957b72ff4a5c46227027cf46115",
+    "url": "/reward-store/static/media/buy-blue.2f11a957.svg"
+  },
+  {
+    "revision": "40bc281346af2ba434a9d9792491715d",
+    "url": "/reward-store/static/media/buy-white.40bc2813.svg"
+  },
+  {
+    "revision": "4669120cae147258c9fc868719b1d3ca",
+    "url": "/reward-store/static/media/coin.4669120c.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/reward-store/static/media/header-x1.f0b98264.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reward-store-morales-andrea/precache-manifest.067d78c3ec729a1b0266e6a5f1ba09f3.js"
+  "/reward-store/precache-manifest.418933044e86e0409185bbf273543398.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reward-store-morales-andrea/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reward-store/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
