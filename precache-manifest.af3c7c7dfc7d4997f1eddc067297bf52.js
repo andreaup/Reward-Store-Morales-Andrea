b274f535cfea7ed367bc3760a60dcbab6e1788cd
kamorales <kamoralescarranza@gmail.com>
2020-09-30 22:00:46 -0500

    Updates

---
+++ precache-manifest.af3c7c7dfc7d4997f1eddc067297bf52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f625ebd8d1b89e73b63924d24e1457",
+    "url": "/Reward-Store-Morales-Andrea/index.html"
+  },
+  {
+    "revision": "fdc9c77b18e73a66f1d5",
+    "url": "/Reward-Store-Morales-Andrea/static/css/main.80374362.chunk.css"
+  },
+  {
+    "revision": "f5d85d7dbc0244c1f8f3",
+    "url": "/Reward-Store-Morales-Andrea/static/js/2.b3672d18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reward-Store-Morales-Andrea/static/js/2.b3672d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc9c77b18e73a66f1d5",
+    "url": "/Reward-Store-Morales-Andrea/static/js/main.919f7693.chunk.js"
+  },
+  {
+    "revision": "90786a6e933aee317fff",
+    "url": "/Reward-Store-Morales-Andrea/static/js/runtime-main.cdc84b70.js"
+  },
+  {
+    "revision": "eafbdf977fa71aaa4f363c15fc79bfc6",
+    "url": "/Reward-Store-Morales-Andrea/static/media/aerolab-logo.eafbdf97.svg"
+  },
+  {
+    "revision": "35b334b79af752bbd6319e3c8830e94c",
+    "url": "/Reward-Store-Morales-Andrea/static/media/buy-blue.35b334b7.svg"
+  },
+  {
+    "revision": "6c4dec8bc6e719ada7ce9ea299cf0c56",
+    "url": "/Reward-Store-Morales-Andrea/static/media/buy-white.6c4dec8b.svg"
+  },
+  {
+    "revision": "da7936e61d9b61fb6a51d5fbf850d6bd",
+    "url": "/Reward-Store-Morales-Andrea/static/media/coin.da7936e6.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/Reward-Store-Morales-Andrea/static/media/header-x1.f0b98264.png"
+  }
+]);
